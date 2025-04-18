7a4a5ee4717a1709ae364a5871980956c38a6a4f
Kris <krishnakumars0101@gmail.com>
2025-04-19 02:49:54 +0530

    (git) Added interview level functions

--- callback.js
+++ callback.js
@@ -1,10 +1,48 @@
-function greet(name,callback){
-    console.log("Hello " + name);
-    callback();
-}
+// function greet(name,callback){
+//     console.log("Hello " + name);
+//     callback();
+// }
 
-function aftergreet(){
-    console.log("welcome to the callback function practice!");
-}
+// function aftergreet(){
+//     console.log("welcome to the callback function practice!");
+// }
 
-greet("jhon",aftergreet)
+// greet("jhon",aftergreet)
+// ------------------------------------
+// setTimeout(function(){
+//     console.log("TImer finished");
+    
+// }, 5000)
+
+// function x(y){
+//     console.log("x");
+//     y();
+    
+// }
+// x(function y(){
+//     console.log("y");
+// })
+// -----------------------------------------
+
+function one() {
+    const a = 10;
+    two();
+  }
+  function two() {
+    const b = 20;
+    console.log("Inside two");
+  }
+  one();
+  
+//   ---------------------------------
+// interview level
+var x = 1;
+function foo() {
+  var x = 2;
+  function bar() {
+    console.log(x);
+  }
+  return bar;
+}
+var baz = foo();
+baz(); // 2, not 1
