edcd993b02ba2ca3ef60a300f6140b4094f2643d
Kris <krishnakumars0101@gmail.com>
2025-04-19 03:10:47 +0530

    git config --global alias.ci commit -m checking this

--- callback.js
+++ callback.js
@@ -13,36 +13,22 @@
 //     console.log("TImer finished");
     
 // }, 5000)
+// --------------------------------------
 
-// function x(y){
-//     console.log("x");
-//     y();
-    
-// }
-// x(function y(){
-//     console.log("y");
-// })
-// -----------------------------------------
 
-function one() {
-    const a = 10;
-    two();
-  }
-  function two() {
-    const b = 20;
-    console.log("Inside two");
-  }
-  one();
-  
-//   ---------------------------------
-// interview level
-var x = 1;
-function foo() {
-  var x = 2;
-  function bar() {
-    console.log(x);
-  }
-  return bar;
+
+// closure
+
+
+
+function outer(){
+    var a =10;
+    function inner(){
+        console.log(a);
+    }
+    return inner;
 }
-var baz = foo();
-baz(); // 2, not 1
+
+outer()()
+
+
