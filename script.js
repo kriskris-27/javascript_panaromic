4a9c8dba0a03f10cfd6064df7471679d303ead3e
Kris <krishnakumars0101@gmail.com>
2025-04-25 11:34:45 +0530

    object

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-console.log('Hello Git');
+// console.log('Hello js');
+// function addtwoval(a,b){
+//     return a+b
+// }
+
+// console.log(addtwoval(2,4));
+
+
+const car = {
+    name:"Audi r8",
+    manuf:"Audi",
+    cc:"2500",
+    bhp:"600",
+    torque:"520",
+    color:"red"
+};
+
+console.log(car)
