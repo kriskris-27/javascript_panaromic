89a75f5617d730db2a7261f90f01a118ff39029d
kris <krishnakumars0101@gmail.com>
2025-05-03 00:04:45 +0530

    stash is good

--- script.js
+++ script.js
@@ -8,3 +8,9 @@ console.log("new feature");
 //TODO
 //everything seems fine 
 //now lets move back to the feat1 
+
+ //my half build code and im not ready to commit in the halfway i need to switch to the main :(
+ 
+ //stash working fine 
+
+ // this feature is awesome
